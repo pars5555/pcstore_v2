b1efe9ab75c168a206a14e684e04215de6617384
pars555 <pars5555@yahoo.com>
2015-03-22 13:58:35 +0400

    aaa

--- bin/cron.php
+++ bin/cron.php
@@ -3,10 +3,10 @@
 error_reporting(E_ALL ^ E_NOTICE);
 ini_set('display_errors', true);
 defined('__DIR__') or define('__DIR__', dirname(__FILE__));
-$_dir = trim(__DIR__, 'htdocs\/bin');
 
+$_SERVER["DOCUMENT_ROOT"] = trim(__DIR__, 'bin');
 $_SERVER['environment'] = 'production';
-require_once ($_dir . "/conf/constants.php");
+require_once ($_SERVER["DOCUMENT_ROOT"] . "/conf/constants.php");
 require_once (CLASSES_PATH . "/framework/DBMSFactory.class.php");
 require_once (CLASSES_PATH . "/managers/OnlineUsersManager.class.php");
 require_once (CLASSES_PATH . "/managers/CbaRatesManager.class.php");
